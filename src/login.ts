dd18f6924c7726e4239abae48d828f0a4e2cffb3
shuymn <mail@shuymn.me>
2021-05-15 10:58:17 +0900

    style(prettier): quick fix

--- src/login.ts
+++ src/login.ts
@@ -11,82 +11,82 @@ import { teprint } from './utils';
  * Login to King of Time
  * @param {string} profile specifying the profile of credential
  */
-export const login = (profile: string): RTE.ReaderTaskEither<Page, Error, Page> => (
-  page: Page
-): TE.TaskEither<Error, Page> =>
-  pipe(
-    getCredential(profile),
-    TE.fromIOEither,
-    TE.chain((credential) => pipe(gotoLoginPage(credential)(page), TE.chain(tryToLogin(credential))))
-  );
+export const login =
+  (profile: string): RTE.ReaderTaskEither<Page, Error, Page> =>
+  (page: Page): TE.TaskEither<Error, Page> =>
+    pipe(
+      getCredential(profile),
+      TE.fromIOEither,
+      TE.chain((credential) => pipe(gotoLoginPage(credential)(page), TE.chain(tryToLogin(credential))))
+    );
 
 const buildLoginPageUrl = (credential: Credential): string => `https://${credential.type}.kingtime.jp/admin`;
 
-const gotoLoginPage = (credential: Credential): RTE.ReaderTaskEither<Page, Error, Page> => (
-  page: Page
-): TE.TaskEither<Error, Page> =>
-  pipe(
-    buildLoginPageUrl(credential),
-    (url) =>
-      pipe(
-        teprint(`going to the login page. URL: ${url}`),
-        TE.chain(() =>
-          TE.tryCatch(
-            () => page.goto(url),
-            () => new Error(ERROR.GO_TO_LOGIN_PAGE(url))
+const gotoLoginPage =
+  (credential: Credential): RTE.ReaderTaskEither<Page, Error, Page> =>
+  (page: Page): TE.TaskEither<Error, Page> =>
+    pipe(
+      buildLoginPageUrl(credential),
+      (url) =>
+        pipe(
+          teprint(`going to the login page. URL: ${url}`),
+          TE.chain(() =>
+            TE.tryCatch(
+              () => page.goto(url),
+              () => new Error(ERROR.GO_TO_LOGIN_PAGE(url))
+            )
           )
+        ),
+      TE.chain<Error, unknown, void>(() => teprint('loading the login page')),
+      TE.chain(() =>
+        TE.tryCatch(
+          () =>
+            Promise.all(
+              [SELECTOR.FORM_LOGIN_ID, SELECTOR.FORM_LOGIN_PASSWORD, SELECTOR.BUTTON_LOGIN].map((selector) =>
+                page.waitForSelector(selector)
+              )
+            ),
+          () => new Error(ERROR.LOAD_LOGIN_PAGE)
         )
       ),
-    TE.chain<Error, unknown, void>(() => teprint('loading the login page')),
-    TE.chain(() =>
-      TE.tryCatch(
-        () =>
-          Promise.all(
-            [SELECTOR.FORM_LOGIN_ID, SELECTOR.FORM_LOGIN_PASSWORD, SELECTOR.BUTTON_LOGIN].map((selector) =>
-              page.waitForSelector(selector)
-            )
-          ),
-        () => new Error(ERROR.LOAD_LOGIN_PAGE)
-      )
-    ),
-    TE.chain(() => TE.taskEither.of(page))
-  );
+      TE.chain(() => TE.taskEither.of(page))
+    );
 
-const tryToLogin = ({ id, password }: Credential): RTE.ReaderTaskEither<Page, Error, Page> => (
-  page: Page
-): TE.TaskEither<Error, Page> =>
-  pipe(
-    teprint(`trying to login. ID: ${id}`),
-    TE.chain(() =>
-      TE.tryCatch(
-        async () => {
-          await page.type(SELECTOR.FORM_LOGIN_ID, id);
-          await page.type(SELECTOR.FORM_LOGIN_PASSWORD, password);
-          await page.click(SELECTOR.BUTTON_LOGIN);
-        },
-        () => new Error(ERROR.TRY_TO_LOGIN)
-      )
-    ),
-    TE.chain(() =>
-      TE.tryCatch(
-        // Successful login: resolves detectTimeCardPage
-        // Failed to login:
-        //   case1. Incorrect credentials
-        //     -> rejects detectLoginFailure and then throws error
-        //   case2. The page transition timed out(Regardless of whether the credentials are correct or not)
-        //     -> rejects detectLoginFailure of detectTimeCardPage and then throws error
-        () => Promise.race([detectLoginFailure(page), detectTimeCardPage(page)]),
-        (reason): Error => {
-          if (reason instanceof Error) {
-            return new Error(ERROR.LOGIN(reason.message));
+const tryToLogin =
+  ({ id, password }: Credential): RTE.ReaderTaskEither<Page, Error, Page> =>
+  (page: Page): TE.TaskEither<Error, Page> =>
+    pipe(
+      teprint(`trying to login. ID: ${id}`),
+      TE.chain(() =>
+        TE.tryCatch(
+          async () => {
+            await page.type(SELECTOR.FORM_LOGIN_ID, id);
+            await page.type(SELECTOR.FORM_LOGIN_PASSWORD, password);
+            await page.click(SELECTOR.BUTTON_LOGIN);
+          },
+          () => new Error(ERROR.TRY_TO_LOGIN)
+        )
+      ),
+      TE.chain(() =>
+        TE.tryCatch(
+          // Successful login: resolves detectTimeCardPage
+          // Failed to login:
+          //   case1. Incorrect credentials
+          //     -> rejects detectLoginFailure and then throws error
+          //   case2. The page transition timed out(Regardless of whether the credentials are correct or not)
+          //     -> rejects detectLoginFailure of detectTimeCardPage and then throws error
+          () => Promise.race([detectLoginFailure(page), detectTimeCardPage(page)]),
+          (reason): Error => {
+            if (reason instanceof Error) {
+              return new Error(ERROR.LOGIN(reason.message));
+            }
+            // Throws unexpected error because it is unknown whether an error other than the Error type will occur.
+            return new Error(ERROR.UNEXPECTED(reason));
           }
-          // Throws unexpected error because it is unknown whether an error other than the Error type will occur.
-          return new Error(ERROR.UNEXPECTED(reason));
-        }
-      )
-    ),
-    TE.chain(() => TE.taskEither.of(page))
-  );
+        )
+      ),
+      TE.chain(() => TE.taskEither.of(page))
+    );
 
 const detectLoginFailure = async (page: Page): Promise<void> => {
   try {
